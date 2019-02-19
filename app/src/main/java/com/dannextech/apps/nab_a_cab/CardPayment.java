e6ff214312df9e4f77380a658cbfb4b8d1313ec2
dannexdaniels <dannex.mwangi@gmail.com>
2019-02-19 22:32:02 +0300

    Added Mpesa Payment processing

--- app/src/main/java/com/dannextech/apps/nab_a_cab/CardPayment.java
+++ app/src/main/java/com/dannextech/apps/nab_a_cab/CardPayment.java
@@ -9,5 +9,6 @@ public class CardPayment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_payment);
+
     }
 }
